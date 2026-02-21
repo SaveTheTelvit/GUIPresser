6d797fcfaf16da4b9e2635424e020a9f3b57f69d
SaveTheTelvit <132656005+SaveTheTelvit@users.noreply.github.com>
2026-02-21 17:44:16 +0300

    Add files via upload

--- addons/GUIPresser/gui_presser.gd
+++ addons/GUIPresser/gui_presser.gd
@@ -1,23 +1,36 @@
 @tool
 extends Node
 
-const BUTTONS: Array[int] = [MOUSE_BUTTON_LEFT, MOUSE_BUTTON_RIGHT, MOUSE_BUTTON_MIDDLE]
+class_name GUIPresser
+
+const MASK: Dictionary = {MOUSE_BUTTON_LEFT : MOUSE_BUTTON_MASK_LEFT, MOUSE_BUTTON_RIGHT : MOUSE_BUTTON_MASK_RIGHT, MOUSE_BUTTON_MIDDLE : MOUSE_BUTTON_MASK_MIDDLE}
 
 signal hover_state_changed(state: bool)
 signal pressed_state_changed(state: bool, button: int)
 signal activated(button: int)
 
 @export var disabled: bool = false : set = _set_disabled
+@export_flags("Button Left", "Button Right", "Button Middle") var button_mask: int = 7 : set = _set_button_mask
 
 var parent: Control = null
 
-var pressed_arr: Array[bool] = [false, false, false]
+var pressed: int = 0
 var hovered: bool = false
 
 func _set_disabled(state: bool) -> void:
 	disabled = state
 	if disabled: _disconnect_parent()
 	else: _connect_parent()
+
+func _set_button_mask(value: int) -> void:
+	var changes: int = (value ^ button_mask)
+	var values: Array = MASK.values()
+	for i in values.size():
+		var m: int = values[i]
+		if changes & m && !(value & m) && pressed & m: 
+			pressed &= ~m
+			pressed_state_changed.emit(false, MASK.keys()[i])
+	button_mask = value
 
 func _disconnect_parent() -> void:
 	if parent:
@@ -63,10 +76,12 @@ func _hover_state_changed(value: bool) -> void:
 		emit_signal("hover_state_changed", value)
 
 func _gui_processing(event: InputEvent) -> void:
-	if event is InputEventMouseButton && BUTTONS.has(event.button_index):
+	if event is InputEventMouseButton && MASK.has(event.button_index):
 		if event.pressed && !hovered: return
-		var arr_index: int = event.button_index - 1
-		if pressed_arr[arr_index] == event.pressed: return
-		pressed_arr[arr_index] = event.pressed
+		var current_mask: int = MASK[event.button_index]
+		if !(button_mask & current_mask): return
+		if bool(pressed & current_mask) == event.pressed: return
+		if event.pressed: pressed |= current_mask
+		else: pressed &= ~current_mask
 		emit_signal("pressed_state_changed", event.pressed, event.button_index)
 		if !event.pressed && hovered: emit_signal("activated", event.button_index)
